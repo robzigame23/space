8d46fcd87ab4da106a7aca3aaded02ca22101961
robzigame23 <92245444+robzigame23@users.noreply.github.com>
2021-11-14 11:37:23 +0300

    first

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1636878915,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/ship-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/fire-sheet0.png",
+		"images/rock-sheet0.png",
+		"images/lose-sheet0.png",
+		"images/win-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/shields-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
